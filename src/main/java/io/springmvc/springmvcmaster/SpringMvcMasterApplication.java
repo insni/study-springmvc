28b76820afe7464c8f4360dd265776d13c7dde15
onjsdnjs <onjsdnjs@gmail.com>
2025-02-11 19:55:41 +0900

    Initial commit

---
+++ src/main/java/io/springmvc/springmvcmaster/SpringMvcMasterApplication.java
@@ -0,0 +1,13 @@
+package io.springmvc.springmvcmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/springmvc/springmvcmaster/SpringMvcMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springmvc.springmvcmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
